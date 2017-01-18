2cd2fd42957e159ef70e42085b3482b1776dcf86
Polo Woo <polo@inipro.net>
2017-01-18 14:52:14 +0900

    Create gpio_test.c

---
+++ gpio_test.c
@@ -0,0 +1,24 @@
+#include "xparameters.h"
+#include "xgpio.h"
+
+int main (void)
+{
+  XGpio switches, leds;
+  int switches_check;
+
+  print("-- Start of the Program --\r\n");
+
+  XGpio_Initialize(&switches, XPAR_SWITCHES_DEVICE_ID);
+  XGpio_SetDataDirection(&switches, 1, 0xffffffff);
+
+  XGpio_Initialize(&leds, XPAR_LEDS_DEVICE_ID);
+  XGpio_SetDataDirection(&leds, 1, 0x00000000);
+
+  while (1)
+  {
+    switches_check = XGpio_DiscreteRead(&switches, 1);
+    XGpio_DiscreteWrite(&leds, 1, switches_check);
+    xil_printf("Switch Status : %x\r\n", switches_check);
+    sleep(1);
+  }
+}
