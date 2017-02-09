ed4d45bd04f1f890789c2e31c4f88493648a60fd
Polo Woo <polo@inipro.net>
2017-02-09 13:20:24 +0900

    Update myip_test.c

--- myip_test.c
+++ myip_test.c
@@ -4,20 +4,20 @@
 
 int main (void)
 {
-	XGpio switches;
-	int switches_check;
-	u32 myip_BaseAddress = XPAR_MYIP_0_S_AXI_BASEADDR;
+  XGpio switches;
+  int switches_check;
+  u32 myip_BaseAddress = XPAR_MYIP_0_S_AXI_BASEADDR;
 
-	xil_printf("-- Start of the Program --\r\n");
+  xil_printf("-- Start of the Program --\r\n");
 
-	XGpio_Initialize(&switches, XPAR_AXI_GPIO_0_DEVICE_ID);
-	XGpio_SetDataDirection(&switches, 1, 0xffffffff);
+  XGpio_Initialize(&switches, XPAR_AXI_GPIO_0_DEVICE_ID);
+  XGpio_SetDataDirection(&switches, 1, 0xffffffff);
 
-	while (1)
-	{
-		switches_check = XGpio_DiscreteRead(&switches, 1);
-		MYIP_mWriteReg(myip_BaseAddress, 0, switches_check);
-		xil_printf("Switch Status : %x\r\n", switches_check);
-		sleep(1);
-	}
+  while (1)
+  {
+    switches_check = XGpio_DiscreteRead(&switches, 1);
+    MYIP_mWriteReg(myip_BaseAddress, 0, switches_check);
+    xil_printf("Switch Status : %x\r\n", switches_check);
+    sleep(1);
+  }
 }
